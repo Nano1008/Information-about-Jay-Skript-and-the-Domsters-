d296a4fc09eda4589ea1faec978a0e319cfce6e0
Nano <nanossn1008@gmail.com>
2022-09-06 21:37:31 +0800

    补充photos界面

--- scripts/global.js
+++ scripts/global.js
@@ -150,17 +150,67 @@ function prepareInternalnav() {
     if (navs.length == 0) return false;
     let nav = navs[0];
     let links = nav.getElementsByTagName("a");
-    for (let i=0;i<links.length;i++) {
+    for (let i = 0; i < links.length; i++) {
         let sectionid = links[i].getAttribute("href").split("#")[1];
         if (!document.getElementById(sectionid)) return false;
         document.getElementById(sectionid).style.display = "none";
         links[i].destination = sectionid;
-        links[i].onclick = function() {
+        links[i].onclick = function () {
             showSection(this.destination);
             return false;
+        }
+    }
+}
+function showPic(whichpic) {
+    if (!document.getElementById("placeholder")) return true;
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    placeholder.setAttribute("src", source);
+    if (!document.getElementById("description")) return false;
+    if (whichpic.getAttribute("title")) {
+        var text = whichpic.getAttribute("title");
+    } else {
+        var text = "";
+    }
+    var description = document.getElementById("description");
+    if (description.firstChild.nodeType == 3) {
+        description.firstChild.nodeValue = text;
+    }
+    return false;
+}
+
+function preparePlaceholder() {
+    if (!document.createElement) return false;
+    if (!document.createTextNode) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id", "placeholder");
+    placeholder.setAttribute("src", "images/placeholder.gif");
+    placeholder.setAttribute("alt", "my image gallery");
+    var description = document.createElement("p");
+    description.setAttribute("id", "description");
+    var desctext = document.createTextNode("Choose an image");
+    description.appendChild(desctext);
+    var gallery = document.getElementById("imagegallery");
+    insertAfter(description, gallery);
+    insertAfter(placeholder, description);
+}
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            return showPic(this);
         }
     }
 }
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
 addLoadEvent(prepareInternalnav);
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
